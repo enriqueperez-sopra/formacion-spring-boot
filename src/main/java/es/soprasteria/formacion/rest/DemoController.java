7aae880ab1c5ee2e30f149295468c50a0c0dfcb4
enriqueperez-sopra <enriquejpo@ext.zara.com>
2020-05-11 11:25:26 +0200

    Integración con Swagger.

--- src/main/java/es/soprasteria/formacion/rest/DemoController.java
+++ src/main/java/es/soprasteria/formacion/rest/DemoController.java
@@ -2,6 +2,10 @@ package es.soprasteria.formacion.rest;
 
 import es.soprasteria.formacion.dto.PersonDto;
 import es.soprasteria.formacion.service.PersonService;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -14,8 +18,13 @@ public class DemoController {
   @Autowired
   private PersonService personService;
 
+  @ApiOperation(value="Este método saluda a un usuario")
+  @ApiResponses(value = {
+      @ApiResponse(code = 200, message = "Llamada ejecutada correctamente"),
+      @ApiResponse(code = 500, message = "Error inesperado"),
+  })
   @GetMapping("/hello/{name}")
-  public String hello(@PathVariable(name="name") String name) {
+  public String hello(@ApiParam(value="El usuario a saludar") @PathVariable(name="name") String name) {
     return "Hello " + name;
   }
 
